397fcc71ea0e066d1f7bb2117c9e41d7b615cab2
CodeAkio <siqueira_2004@hotmail.com>
2024-05-13 00:21:29 -0300

    feat: user authentication

--- setup/settings.py
+++ setup/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = "setup.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -125,3 +125,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # Custom user
 AUTH_USER_MODEL = "accounts.CustomUser"
+
+# Login
+LOGIN_REDIRECT_URL = "home"
+LOGOUT_REDIRECT_URL = "home"
