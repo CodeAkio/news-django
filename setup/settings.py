34c0f14484c66a20972c5bec8f4ff6f305d0c261
CodeAkio <siqueira_2004@hotmail.com>
2024-07-14 19:49:14 -0300

    feat: add change and reset password

--- setup/settings.py
+++ setup/settings.py
@@ -138,3 +138,6 @@ LOGOUT_REDIRECT_URL = "home"
 # Crispy
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 CRISPY_TEMPLATE_PACK = "bootstrap5"
+
+# E-mail
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
